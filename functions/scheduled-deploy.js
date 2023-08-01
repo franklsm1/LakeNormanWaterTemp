15a4886bf2767587bb19deddff86efd00d87cb52
Sean Franklin <sfrbf@allstate.com>
2023-07-31 21:54:02 -0400

    add log to scheduled function

--- functions/scheduled-deploy.js
+++ functions/scheduled-deploy.js
@@ -4,13 +4,14 @@ import { schedule } from '@netlify/functions'
 const BUILD_HOOK = 'https://api.netlify.com/build_hooks/64c79195d9ea9602dcf62dc3'
 
 // Schedules the app to rebuild every 15 minutes to update the temp
-const handler = schedule('*/15 * * * *', async () => {
+const handler = schedule('*/15 * * * *', async (event, context) => {
+    console.log("Received event:", event)
     await axios.post(BUILD_HOOK).then(response => {
         console.log('Build hook response:', response)
     })
 
     return {
-        statusCode: 200
+        statusCode: 200,
     }
 })
 
