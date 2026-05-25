4f632062d89e3bfa10f9233a3d50379a831e94fe
Sean Franklin <franklsm1@gmail.com>
2026-05-25 08:51:47 -0400

    Add lake level

--- functions/getWaterTemperature.js
+++ functions/getWaterTemperature.js
@@ -12,26 +12,21 @@ exports.handler = async (event, context, callback) => {
         const response = await axios.post(temperatureApi,temperatureRequestBody)
             .then(res => res.data);
         const tempCelsius = (response.d.temp_degC).toFixed(1);
-        if (tempCelsius > 1 && tempCelsius < 38 ) {
-            const time = response.d.time.replace('-04:00', '-00:00').replace('-05:00', '-00:00');
-            const lastReadDateTime = new Date(time);
-            const currentDate = new Date();
-            const timeDifference = ((currentDate - lastReadDateTime) / (1000 * 60)).toFixed(0);
-            const tempFahrenheit = (tempCelsius * (9/5) + 32).toFixed(1);
-            tempResponse = {
-                tempFahrenheit,
-                tempCelsius,
-                timeDifference
-            };
-            console.log('temp response:', tempResponse);
-            fallbackResponse = tempResponse;
-        } else if (fallbackResponse) {
-            console.log('out of range using fallback temp response:', fallbackResponse);
-            tempResponse = fallbackResponse;
-        } else {
-            callback(null, { statusCode: 500 });
-            return;
-        }
+        const time = response.d.time.replace('-04:00', '-00:00').replace('-05:00', '-00:00');
+        const lastReadDateTime = new Date(time);
+        const currentDate = new Date();
+        const timeDifference = ((currentDate - lastReadDateTime) / (1000 * 60)).toFixed(0);
+        const tempFahrenheit = (tempCelsius * (9/5) + 32).toFixed(1);
+        const lakeLevel = await getLakeLevel()
+
+        tempResponse = {
+            tempFahrenheit,
+            tempCelsius,
+            timeDifference,
+            lakeLevel
+        };
+        console.log('temp response:', tempResponse);
+        fallbackResponse = tempResponse;
     }  catch (err) {
         console.log('err:', err);
         if (fallbackResponse) {
@@ -46,4 +41,33 @@ exports.handler = async (event, context, callback) => {
         body: JSON.stringify(tempResponse),
     });
     return tempResponse;
+}
+async function getLakeLevel() {
+  try {
+    const response = await axios.get(
+      "https://api.hydro-derived.duke-energy.app/lakes/current-level",
+      {
+        headers: {
+          accept: "application/json, text/plain, */*",
+          origin: "https://lakes.hydro-derived.duke-energy.app"
+        }
+      }
+    );
+
+    const lakes = response.data;
+
+    // Find the lake with LakeId = 4
+    const lake = lakes.find(l => l.LakeId === 4);
+
+    if (!lake) {
+      console.log("LakeId 4 not found");
+      return;
+    }
+
+    console.log("Actual value for LakeId 4:", lake.Actual);
+    return lake.Actual;
+
+  } catch (err) {
+    console.error("Error fetching lake data:", err);
+  }
 }
